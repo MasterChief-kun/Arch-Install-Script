bf691734e3acfb13e52fb4d4a1c309e692688208
MasterChief-kun <70462175+MasterChief-kun@users.noreply.github.com>
2020-12-07 20:15:58 +0530

    Fixed /etc/hosts bug

--- Arch-pt2.sh
+++ Arch-pt2.sh
@@ -17,7 +17,7 @@ read hostname
 echo $hostname >> /etc/hostname
 echo "127.0.0.1          localhost
 ::1                localhost
-127.0.1.1          $hostname.localdomain     $hostname"
+127.0.1.1          $hostname.localdomain     $hostname" >> /etc/hosts
 
 echo Please set root password
 passwd
