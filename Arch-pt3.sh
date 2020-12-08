4329e10fd696db58146a5748e5962583847ec3d7
MasterChief-kun <70462175+MasterChief-kun@users.noreply.github.com>
2020-12-08 13:44:09 +0530

    Delete Arch-pt3.sh

--- Arch-pt3.sh
+++
@@ -1,6 +0,0 @@
-echo Please enter diskname:
-read diskname
-1="1"
-mount $diskname${1} /boot/EFI
-grub-install --target=x86_64-efi --bootloader-id=mygrub --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
