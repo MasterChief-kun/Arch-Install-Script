a623142fa4cd64f45725a07ddbcb5571458f2a94
MasterChief-kun <70462175+MasterChief-kun@users.noreply.github.com>
2020-12-08 13:33:14 +0530

    Update Arch-pt2.sh

--- Arch-pt2.sh
+++ Arch-pt2.sh
@@ -32,7 +32,7 @@ sed -i '82 s/^##*//' /etc/sudoers
 pacman -S grub
 pacman -S dosfstools efibootmgr mtools os-prober
 mkdir /boot/EFI
-echo Please enter EFI partition(i.e: diskname1; eg:/dev/sda1)
+echo "Please enter EFI partition(i.e: diskname1; eg:/dev/sda1)"
 read efi
 grub-install --target=x86_64-efi --bootloader-id=mygrub --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
